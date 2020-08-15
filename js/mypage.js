a7ff9e48fb57f790346a7661dd85515b59596c5a
Christian Eckardt <eckardt.christian@web.de>
2020-08-15 11:36:54 +0200

    thats nice

--- js/mypage.js
+++ js/mypage.js
@@ -1,3 +1,12 @@
 function closeBanner(){
   document.getElementById('cookies').style.display="none";
 }
+
+function showMenu(){
+  if (document.getElementById("burger-menu").style.display === "none") {
+    document.getElementById("burger-menu").style.display = "block";
+  }
+  else {
+    document.getElementById("burger-menu").style.display = "none";
+  }
+}
