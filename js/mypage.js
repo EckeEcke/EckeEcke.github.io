dd63b156c839f99fe9a5ff4b919220a38da82301
Christian Eckardt <eckardt.christian@web.de>
2020-10-09 20:22:21 +0200

    improving css page

--- js/mypage.js
+++ js/mypage.js
@@ -1,7 +1,3 @@
-function closeBanner(){
-  document.getElementById('cookies').style.display="none";
-}
-
 function showMenu(){
   if (document.getElementById("burger-menu").style.display === "none") {
     document.getElementById("burger-menu").style.display = "block";
