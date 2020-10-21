1202de6cefd46b460bb63a41a0b1aa9505814232
Christian Eckardt <eckardt.christian@web.de>
2020-10-21 14:43:41 +0200

    improving css page

--- js/travelblog.js
+++ js/travelblog.js
@@ -19,7 +19,6 @@ const emoji3Button = document.getElementById("🙂");
 const emoji4Button = document.getElementById("😒");
 const emoji5Button = document.getElementById("😡");
 
-
 const editBtn = document.getElementById("edit-button");
 const editableCity = document.getElementsByClassName("city");
 const editableCountry = document.getElementsByClassName("country");
@@ -28,15 +27,16 @@ const editablePurpose = document.getElementsByClassName("purpose");
 const images = document.getElementsByClassName("image");
 const imageLinks = document.getElementsByClassName("image-link");
 
-
-
 let transport = "";
 let mood = "";
+
+
 
 
 window.onload = function(){
   putInHTML();
 };
+
 
 form.addEventListener("submit", onFormSubmit);
 
@@ -58,7 +58,6 @@ function onFormSubmit(event){
 
 function putInHTML(){
   let entries = getEntries();
-  console.log(entries);
   entries.forEach(createEntry);
 
 };
@@ -74,120 +73,16 @@ function createEntry(entry, index){
       let feelsLike = "Feels like: " + data.main.feels_like  + "°C";
       let weatherDescription = data.weather[0].description;
       let icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      let deleteButton = document.createElement("button");
-      let editButton = document.createElement("button");
-      let editSaveButton = document.createElement("button");
-      let editEmoji = "🖍️";
 
-      blogEntry.innerHTML =
-        `
-        <h3><span class="city edit" contentEditable="false">${entry.city}</span>, <span class="country edit" contentEditable="false">${entry.country}</span>
-        <br>
-        <span class="time edit" contentEditable="false">${entry.travelTime}</span></h3>
-
-        <img class="blog-image" src="${entry.image}">
-
-        <span class="image-link" style="display: none;">Enter new image link: <br><span class="image edit" contentEditable>${entry.image}</span></span>
-
-        <br><br>
-        <p class="purpose edit" contentEditable="false">${entry.purpose}</p>
-
-        <hr class="blog-line">
-
-        <p>Transport: <span class="transport">${entry.transport}</span>&nbsp; &nbsp;
-        Mood: <span class="mood">${entry.mood}</span></p>
-
-        <span>
-        ${temp}
-        <br>
-        ${feelsLike}
-        <br>
-        ${weatherDescription}
-        <br>
-        <img class="weather-icon" src="${icon}">
-        <br>
-        </span>
-        <br>
-        `;
-
-      console.log(blogEntry);
-      deleteButton.innerHTML = "🗑️";
-      deleteButton.classList.add("delete-button");
-      blogEntry.appendChild(deleteButton);
-      deleteButton.addEventListener("click", function() {
-        deleteEntry(index);
-        putInHTML();
+      createEntryContent(blogEntry, entry, temp, feelsLike, weatherDescription, icon);
+      createDeleteBtn(blogEntry, index);
+      createEditBtn(blogEntry, index);
+      createEditSaveBtn(blogEntry, entry, index);
+      blogEntries.appendChild(blogEntry);
       });
 
-      editButton.innerHTML = `<span class="edit-icon">${editEmoji} </span>`;
-      editButton.classList.add("edit-button");
-      blogEntry.appendChild(editButton);
-
-      editButton.addEventListener("click", function(){
-          if(editableCity[index].contentEditable === "false"){
-             editableCity[index].contentEditable = 'true';
-          } else{
-            editableCity[index].contentEditable = "false";
-          };
-
-          if(editableCountry[index].contentEditable === "false"){
-              editableCountry[index].contentEditable = "true";
-          }else{
-            editableCountry[index].contentEditable = "false";
-          };
-
-          if(editableTime[index].contentEditable === "false"){
-              editableTime[index].contentEditable = "true";
-          }else{
-            editableTime[index].contentEditable = "false";
-          };
-
-
-          if(editablePurpose[index].contentEditable === "false"){
-              editablePurpose[index].contentEditable = "true";
-          }else{
-            editablePurpose[index].contentEditable = "false";
-          };
-
-          if(imageLinks[index].style.display == "none"){
-             imageLinks[index].style.display = "block";
-           }else{
-             imageLinks[index].style.display = "none";
-           };
-        });
-
-        editSaveButton.innerHTML = `💾`;
-        editSaveButton.classList.add("edit-save-button");
-        blogEntry.appendChild(editSaveButton);
-        editSaveButton.addEventListener("click", function(){
-          let mood = entry.mood;
-          let transport = entry.transport;
-          let country = editableCountry[index].textContent;
-          let city = editableCity[index].textContent;
-          let purpose = editablePurpose[index].textContent;
-          let image = images[index].textContent;
-          if (image === ""){
-            image = "images/blog-placeholder.jpg";
-          }
-          let travelTime = editableTime[index].textContent;
-          let singleEntry = {city, country, travelTime, purpose, image, mood, transport};
-          let entries = getEntries();
-          entries.splice(index, 1, singleEntry);
-          stringifiedEntries = JSON.stringify(entries);
-          localStorage.setItem("entries", stringifiedEntries);
-
-          editableCity[index].contentEditable = "false";
-          editableCountry[index].contentEditable = "false";
-          editableTime[index].contentEditable = "false";
-          editablePurpose[index].contentEditable = "false";
-          imageLinks[index].style.display = "none";
-          location.reload();
-        });
-
-
-      blogEntries.appendChild(blogEntry);
-});
 };
+
 
 
 
@@ -240,6 +135,127 @@ function saveEntries(singleEntry){
 }
 
 
+
+function createEntryContent(element, entry, temperature, feltTemp, weatherDescriptionText, weatherIcon){
+  element.innerHTML =
+    `
+    <h3><span class="city edit" contentEditable="false">${entry.city}</span>, <span class="country edit" contentEditable="false">${entry.country}</span>
+    <br>
+    <span class="time edit" contentEditable="false">${entry.travelTime}</span></h3>
+
+    <img class="blog-image" src="${entry.image}">
+
+    <span class="image-link" style="display: none;">Enter new image link: <br><span class="image edit" contentEditable>${entry.image}</span></span>
+
+    <br><br>
+    <p class="purpose edit" contentEditable="false">${entry.purpose}</p>
+
+    <hr class="blog-line">
+
+    <p>Transport: <span class="transport">${entry.transport}</span>&nbsp; &nbsp;
+    Mood: <span class="mood">${entry.mood}</span></p>
+
+    <span>
+    ${temperature}
+    <br>
+    ${feltTemp}
+    <br>
+    ${weatherDescriptionText}
+    <br>
+    <img class="weather-icon" src="${weatherIcon}">
+    <br>
+    </span>
+    <br>
+    `;
+}
+
+
+function createDeleteBtn(element, index){
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "🗑️";
+  deleteButton.classList.add("delete-button");
+  element.appendChild(deleteButton);
+  deleteButton.addEventListener("click", function() {
+    deleteEntry(index);
+    putInHTML();
+})};
+
+
+function createEditBtn(element, index){
+  let editButton = document.createElement("button");
+  editButton.innerHTML = `<span class="edit-icon">🖍️</span>`;
+  editButton.classList.add("edit-button");
+  element.appendChild(editButton);
+
+  editButton.addEventListener("click", function(){
+      if(editableCity[index].contentEditable === "false"){
+         editableCity[index].contentEditable = 'true';
+      } else{
+        editableCity[index].contentEditable = "false";
+      };
+
+      if(editableCountry[index].contentEditable === "false"){
+          editableCountry[index].contentEditable = "true";
+      }else{
+        editableCountry[index].contentEditable = "false";
+      };
+
+      if(editableTime[index].contentEditable === "false"){
+          editableTime[index].contentEditable = "true";
+      }else{
+        editableTime[index].contentEditable = "false";
+      };
+
+
+      if(editablePurpose[index].contentEditable === "false"){
+          editablePurpose[index].contentEditable = "true";
+      }else{
+        editablePurpose[index].contentEditable = "false";
+      };
+
+      if(imageLinks[index].style.display == "none"){
+         imageLinks[index].style.display = "block";
+       }else{
+         imageLinks[index].style.display = "none";
+       };
+    });
+}
+
+
+
+function createEditSaveBtn(element, entry, index){
+  let editSaveButton = document.createElement("button");
+  editSaveButton.innerHTML = `💾`;
+  editSaveButton.classList.add("edit-save-button");
+  element.appendChild(editSaveButton);
+  editSaveButton.addEventListener("click", function(){
+    let mood = entry.mood;
+    let transport = entry.transport;
+    let country = editableCountry[index].textContent;
+    let city = editableCity[index].textContent;
+    let purpose = editablePurpose[index].textContent;
+    let image = images[index].textContent;
+    if (image === ""){
+      image = "images/blog-placeholder.jpg";
+    }
+    let travelTime = editableTime[index].textContent;
+    let singleEntry = {city, country, travelTime, purpose, image, mood, transport};
+    let entries = getEntries();
+    entries.splice(index, 1, singleEntry);
+    stringifiedEntries = JSON.stringify(entries);
+    localStorage.setItem("entries", stringifiedEntries);
+
+    editableCity[index].contentEditable = "false";
+    editableCountry[index].contentEditable = "false";
+    editableTime[index].contentEditable = "false";
+    editablePurpose[index].contentEditable = "false";
+    imageLinks[index].style.display = "none";
+    location.reload();
+})};
+
+
+
+
 function checkRadio() {
 
     if (planeButton.checked){
@@ -278,7 +294,9 @@ function checkRadio() {
     return mood;
     return transport;
 
-  }
+  };
+
+
 
 function saveEdits(index){
   console.log(index);
@@ -298,4 +316,4 @@ function saveEdits(index){
   console.log(entries);
   stringifiedEntries = JSON.stringify(entries);
   localStorage.setItem("entries", stringifiedEntries);
-}
+};
