25bdda49ef48d5e9219db04dc6954aa409ba7629
Christian Eckardt <eckardt.christian@web.de>
2020-11-01 15:33:03 +0100

    adding mario2

--- js/jsdemo.js
+++ js/jsdemo.js
@@ -242,3 +242,49 @@ const mario = document.getElementById("mario");
 
     setInterval(moveMario, 100);
     setInterval(moveBackground, 10);
+
+
+
+
+
+
+    const jumpButton = document.getElementById("jump-button");
+   const jumpingBox = document.getElementById("jumping-box");
+   const jumpman = document.getElementById("jumping-mario");
+   const questionBlock = document.getElementById("question-block");
+   const coinSound = document.getElementById("coin");
+
+   let jumpingBoxPosition = 0;
+   let jumpDirection = 12;
+
+
+   let jumpAnimation;
+
+   function makeTheJump(){
+     jumpAnimation = setInterval(jump, 50);
+   }
+
+   function jump(){
+     jumpButton.disabled = true;
+     jumpman.style.left = "-50px";
+     if(jumpingBoxPosition >= 30){
+       jumpman.style.left = "-100px";
+     }
+     if (jumpDirection == 3){
+       questionBlock.style.top = "-2px";
+       coinSound.play();
+     }
+
+     if (jumpDirection == -3){
+       questionBlock.style.top = "7px";
+     }
+     if (jumpingBoxPosition == 0 && jumpDirection < 0){
+       clearInterval(jumpAnimation);
+       jumpDirection = 12;
+       jumpman.style.left = "0px";
+       jumpButton.disabled = false;
+     }
+     jumpingBox.style.bottom = jumpingBoxPosition + "px";
+     jumpDirection -= 1;
+     jumpingBoxPosition += jumpDirection;
+     }
