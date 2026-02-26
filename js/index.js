ef1b7e2b514fd77c7531bb61044eab518ade895f
Christian Eckardt <eckardt.christian@web.de>
2026-02-26 16:09:07 +0100

    add header shadow on scroll

--- js/index.js
+++ js/index.js
@@ -187,3 +187,13 @@ const initStars = () => {
 }
 
 document.addEventListener('DOMContentLoaded', initStars)
+
+const header = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    header.classList.add('scrolled')
+  } else {
+    header.classList.remove('scrolled')
+  }
+})
